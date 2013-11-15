dd22e99a881361a09ca2abee9c216b585c42f1ce
Ariel Kirkwood <ariel.kirkwood@me.com>
2013-11-14 23:25:59 -0500

    rails _3.2.15_ new LANcenter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LANcenter::Application.config.secret_token = 'b9bf3f660c0ee0e7eca06b96263c8cdacdf89b9c1280eef75db544f400ccb7b2cfdc2afe4e5c5f9bab6e7a738026966a46fa342bbcbe704781de7adb436178a6'
