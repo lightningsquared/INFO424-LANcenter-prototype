7eb56317f0f080d207634aebe646e8f39c1ca9de
Ariel Kirkwood <ariel.kirkwood@me.com>
2013-12-02 14:50:55 -0500

    no tests or assets on scaffold generation pls

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,14 @@ end
 
 module LANcenter
   class Application < Rails::Application
+    config.generators do |g|
+      g.view_specs = false
+      g.controller_specs = false
+      g.helper_specs = false
+      g.stylesheets = false
+      g.javascripts = false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
